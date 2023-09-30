64a72b9cb5a4e85b9902e36e4f907e2cf9a7cad6
cupcakequeen77777 <86316346+cupcakequeen77777@users.noreply.github.com>
2023-09-30 18:17:33 +0000

    Add Search Bar Component

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -10,6 +10,9 @@ function Navbar() {
   return (
     <div className="navbar">
       <img src="Logo_no_background2.png" alt="UPS Logo" className="logo"></img>
+      <form>
+        <input type="text" name="search" placeholder="Search.."></input>
+      </form>
       <div className="navbar__account">
         {loggedIn() === false ? (
           <>
