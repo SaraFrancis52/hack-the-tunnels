9ef2d1fb95a3cb428fe1cc2ddc446eef2b8d8421
SaraFrancis52 <79770128+SaraFrancis52@users.noreply.github.com>
2023-09-30 19:54:50 +0000

    Added signup form

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { Page } from "../../components";
 import "./SignUp.style.scss";
 
 function SignUp() {
+  const [emailValue, setEmailValue] = useState("");
+  const [passValue, setPassValue] = useState("");
+
+  const api = async () => {
+    alert(emailValue);
+    alert(passValue);
+  }
+
   return (
     <Page>
       <div className="signup-page">
         <h1>Sign Up</h1>
-      </div>
+          <label>Your email:</label>
+          <br></br>
+          <input id="email" type="email" name="email" onChange={e => setEmailValue(e.target.value)}></input>
+          <br></br>
+          <label>Your password:</label>
+          <br></br>
+          <input id="password" type="password" name="pass" onChange={e => setPassValue(e.target.value)}></input>
+          <br></br>
+          <button className="button" onClick={() => api()}>
+            Sign Up
+          </button>
+        </div>
     </Page>
   );
 }
